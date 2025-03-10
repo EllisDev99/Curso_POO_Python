d40d48069cd96fdfc9e17f50ef562e9e8c1c5b02
Brayan Ellis <ellis@localhost.localdomain>
2025-03-09 20:58:44 -0400

    feat: probando pyfiglet

--- clases/01_clases_&_objetos_.py
+++ clases/01_clases_&_objetos_.py
@@ -1,5 +1,10 @@
 # Las clases son plantillas de como se debe hacer un objeto
 
+from pyfiglet import Figlet
+
+tittle = Figlet(font='sblood')
+subtittle = Figlet(font='pepper')
+
 class Celular(): # Las clases se definen en PascalCase
     # Todos los atributos de la clase se definen aka
     # estos atributos serán compartidos por todos los objetos que instaciemos a partir de esta clase
@@ -8,6 +13,11 @@ class Celular(): # Las clases se definen en PascalCase
     camara = '48MP'
 
 celu1 = Celular() # Cada que creamos un objeto estamos creando una instancia de la clase
+
+print('-'*70)
+print(tittle.renderText('POO'))
+print(subtittle.renderText('CLASES'))
+print('-'*70)
 print(celu1)
 print(celu1.marca)
 print(celu1.modelo)
