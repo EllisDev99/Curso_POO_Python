0bfb0f84b5bd7da2324602500e214307b5f700f0
Brayan Ellis <ellis@localhost.localdomain>
2025-03-09 21:24:23 -0400

    feat: diseñando el título de la clase con pyfiglet

--- clases/01_clases_&_objetos_.py
+++ clases/01_clases_&_objetos_.py
@@ -2,8 +2,17 @@
 
 from pyfiglet import Figlet
 
-tittle = Figlet(font='sblood')
-subtittle = Figlet(font='pepper')
+tittle = 'POO'
+subtittle = '- Clases -'
+
+def tittle_format(tittle, subtittle):
+    format_tittle = Figlet(font='sblood', justify='center')
+    format_subtittle = Figlet(font='pepper', justify='center')
+
+    print('-'*(len(subtittle)*8))
+    print(format_tittle.renderText(tittle.upper()))
+    print(format_subtittle.renderText(subtittle.upper()))
+    print('-'*(len(subtittle)*8))
 
 class Celular(): # Las clases se definen en PascalCase
     # Todos los atributos de la clase se definen aka
@@ -14,10 +23,8 @@ class Celular(): # Las clases se definen en PascalCase
 
 celu1 = Celular() # Cada que creamos un objeto estamos creando una instancia de la clase
 
-print('-'*70)
-print(tittle.renderText('POO'))
-print(subtittle.renderText('CLASES'))
-print('-'*70)
+tittle_format(tittle, subtittle)
+
 print(celu1)
 print(celu1.marca)
 print(celu1.modelo)
