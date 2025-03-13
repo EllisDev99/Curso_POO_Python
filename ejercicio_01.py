bf5cb4ac10b470e4dd7eb36a8bb2081e3f00d85d
Brayan Ellis <ellis@localhost.localdomain>
2025-03-12 22:01:38 -0400

    doc: escribiendo la documentación para el ejercicio.

---
+++ ejercicio_01.py
@@ -0,0 +1,11 @@
+"""
+Crear la clase 'Estudiante', con lo siguientes atriutos: 
+* Nombre 
+* Edad 
+* Grado
+
+además de crear un Método 'estudiar()' -> el estudiante (nombre) está estudiando
+
+El objetivo es crear un objeto 'Estudiante' y usar el método 'estudiar()', se debe
+interactuar con el usuario y este dene brindar los atributos.
+"""
