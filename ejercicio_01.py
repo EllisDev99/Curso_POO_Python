9607c684fe0052f620e3ac5666e6987a85e645b0
Brayan Ellis <ellis@localhost.localdomain>
2025-03-13 21:42:34 -0400

    feat: primer intento de resover ejercicio

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -7,5 +7,15 @@ Crear la clase 'Estudiante', con lo siguientes atriutos:
 además de crear un Método 'estudiar()' -> el estudiante (nombre) está estudiando
 
 El objetivo es crear un objeto 'Estudiante' y usar el método 'estudiar()', se debe
-interactuar con el usuario y este dene brindar los atributos.
+interactuar con el usuario y este debe brindar los atributos.
 """
+class Estudiante:
+    def __init__(self, nombre, edad, grado):
+        self.nombre = nombre
+        self.edad = edad
+        self.grado = grado
+    
+    def estudiar(nombre):
+        print(f'El estudiante {self.nombre} de {self.edad} del grado {self.grado}')
+
+
