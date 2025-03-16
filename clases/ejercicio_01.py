e6200a108892aa1b5101ffe9feade49b37702635
Brayan Ellis <ellis@localhost.localdomain>
2025-03-16 17:07:41 -0400

    feat: intento de validar los inputs.

---
+++ clases/ejercicio_01.py
@@ -0,0 +1,52 @@
+"""
+Crear la clase 'Estudiante', con lo siguientes atriutos: 
+* Nombre 
+* Edad 
+* Grado
+
+además de crear un Método 'estudiar()' -> el estudiante (nombre) está estudiando
+
+El objetivo es crear un objeto 'Estudiante' y usar el método 'estudiar()', se debe
+interactuar con el usuario y este debe brindar los atributos.
+"""
+class Estudiante:
+    def __init__(self, nombre, edad, semestre):
+        self.nombre = nombre
+        self.edad = edad
+        self.semestre = semestre
+    
+    def estudiar():
+        print('Estudiando')
+        
+
+while True:
+    """
+    # Validamos el nombre del estudiante
+    name = str(input('nombre del estudiante: ').strip().upper())
+    while name.isdigit():
+        print('Eso no es un nombre.')
+        name = str(input('nombre del estudiante: ').strip().upper())
+
+ 
+    # Validamos la edad
+    try:
+        edad = int(input('Edad del estudiante: '))
+    except (ValueError, TypeError):
+        print('hay un error con el tipo de dato que indico')
+
+    # Validamos el número del semestre
+    try:
+        semestre = int(input('Indique su semestre\nEjemplo: 8\n: '))
+        while not semestre > 0 or not semestre <= 9:
+            print('Fuera de rango.')
+            semestre = int(input('Indique su semestre\nEjemplo: 8\n: '))
+    except (ValueError, TypeError):
+        print('hay un error con el tipo de dato que indico
+    """
+
+
+    
+    break
+print('FIN')
+
+    
