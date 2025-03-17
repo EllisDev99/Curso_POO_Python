b1194873e7e1984d53d1a87e3280f90f35a50ace
Brayan Ellis <ellis@localhost.localdomain>
2025-03-16 21:01:54 -0400

    feat: validamos el nombre y la edad, además de importar la función tittle_format.

--- clases/ejercicio_01.py
+++ clases/ejercicio_01.py
@@ -9,6 +9,8 @@ además de crear un Método 'estudiar()' -> el estudiante (nombre) está estudia
 El objetivo es crear un objeto 'Estudiante' y usar el método 'estudiar()', se debe
 interactuar con el usuario y este debe brindar los atributos.
 """
+from recursos import tittle_format as tf
+
 class Estudiante:
     def __init__(self, nombre, edad, semestre):
         self.nombre = nombre
@@ -19,33 +21,32 @@ class Estudiante:
         print('Estudiando')
         
 
-while True:
-    """
-    # Validamos el nombre del estudiante
-    name = str(input('nombre del estudiante: ').strip().upper())
-    while name.isdigit():
-        print('Eso no es un nombre.')
-        name = str(input('nombre del estudiante: ').strip().upper())
+# Main
+tf('ejercicios I')
 
- 
+while True:
+    # Validamos el nombre del estudiante
+    name = str(input('Digite su nombre: ')).strip().upper()
+    if name.isdigit():
+        while True:
+            print('Eso no es un nombre.\nDigite su nombre real.')
+            name = str(input('Digite su nombre: ')).strip().upper()
+            if not name.isdigit():
+                break
+    
     # Validamos la edad
     try:
-        edad = int(input('Edad del estudiante: '))
+        edad = int(input('Digite su edad: '))
+        if edad < 18 or edad > 80:
+            while True:
+                print('Edad fuera de rango.')
+                edad = int(input('Digite su edad: '))
+                if edad > 18 and edad < 80:
+                    break
     except (ValueError, TypeError):
-        print('hay un error con el tipo de dato que indico')
+        print('Error por tipo de dato.\nDigite un número entero.')
 
-    # Validamos el número del semestre
-    try:
-        semestre = int(input('Indique su semestre\nEjemplo: 8\n: '))
-        while not semestre > 0 or not semestre <= 9:
-            print('Fuera de rango.')
-            semestre = int(input('Indique su semestre\nEjemplo: 8\n: '))
-    except (ValueError, TypeError):
-        print('hay un error con el tipo de dato que indico
-    """
-
-
-    
+        
     break
 print('FIN')
 
