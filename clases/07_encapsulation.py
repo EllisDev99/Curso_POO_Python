7abfdcb9baa23b44934d4d6f6378f31ad27fdfc5
Brayan Ellis <ellis@localhost.localdomain>
2025-03-23 16:07:38 -0400

    feat: terminando la clase sobre getters y setters.

--- clases/07_encapsulation.py
+++ clases/07_encapsulation.py
@@ -17,5 +17,25 @@ tf('encapsulation')
 obj = Miclase()
 print(obj._atributo_privado)
 #print(obj.__atributo_muy_privado)
-
 obj._metodo_privado()
+
+tf('getters and setters')
+
+class Datos:
+    def __init__(self, nombre, edad):
+        self.__nombre = nombre
+        self._edad = edad
+
+    def get_nombre(self): # Con get accedemos al atributo que técnicamente es privado
+        return self.__nombre
+    
+    def set_nombre(self, nombre): # Con set definimos el valor del atributo nombre que ess provido
+        self.__nombre = nombre
+
+
+persona = Datos('Brayan', 25)
+nombre = persona.get_nombre()
+print(nombre)
+
+persona.set_nombre('Ellis')
+print(persona.get_nombre())
