17cc6794f533f213ef4e8daf414b07fbf9a35af0
Brayan Ellis <ellis@localhost.localdomain>
2025-03-26 20:20:33 -0400

    feat: desarrollando la clase, se crea la función decorador y se crea la función greetings

--- clases/08_decorators.py
+++ clases/08_decorators.py
@@ -6,3 +6,20 @@ función que ya existía.
 
 Los decoradores son utilizados para la validación de entradas, manejo de excepciones, etc.
 """
+
+from recursos import tittle_format as tf
+
+def decorator(funct):
+    def function_modificed():
+        print('Antes de llamar a la función.')
+        funct()
+    return function_modificed
+
+@decorator
+def greetings():
+    print('Hola Ellis, ¿cómo estás? ')
+
+
+tf('decorators')
+
+greetings()
