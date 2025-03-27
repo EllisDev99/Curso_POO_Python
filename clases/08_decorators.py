c484a55d642377cf9005fe0f85c94680ee779ef4
Brayan Ellis <ellis@localhost.localdomain>
2025-03-26 20:02:55 -0400

    doc: desarrollando concepto sobre decoradoes.

---
+++ clases/08_decorators.py
@@ -0,0 +1,8 @@
+"""
+Decorators:
+
+Los decoradores son funciones que decoran a otra, es decir, agregan código extra a una
+función que ya existía.
+
+Los decoradores son utilizados para la validación de entradas, manejo de excepciones, etc.
+"""
