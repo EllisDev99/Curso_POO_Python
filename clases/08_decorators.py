af65d955833b83fbb14a3b71a9d1604bbaced9c2
Brayan Ellis <ellis@localhost.localdomain>
2025-03-26 20:27:46 -0400

    doc: documentación de las funciones

--- clases/08_decorators.py
+++ clases/08_decorators.py
@@ -6,17 +6,29 @@ función que ya existía.
 
 Los decoradores son utilizados para la validación de entradas, manejo de excepciones, etc.
 """
-
 from recursos import tittle_format as tf
 
 def decorator(funct):
+    """
+    Decorador que modifica el comportamiento de una función:
+    1. Imprime un mensaje antes de ejecutar la función original.
+    2. Llama a la función original.
+    
+    Args:
+        funct (function): Función a decorar.
+    
+    Returns:
+        function: Función modificada que incluye el nuevo comportamiento.
+    """
     def function_modificed():
-        print('Antes de llamar a la función.')
-        funct()
+        print('Antes de llamar a la función.') # Acción adicional antes de la función original
+        funct() # Llamada a la función original
     return function_modificed
 
-@decorator
+
+@decorator # Aplicamos el decorador a la función 'greetings'
 def greetings():
+    """Función que imprime un saludo personalizado."""
     print('Hola Ellis, ¿cómo estás? ')
 
 
