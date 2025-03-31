ef373775537eecdd4700272256148b07c6548060
Brayan Ellis <ellis@localhost.localdomain>
2025-03-31 19:49:34 -0400

    feat: terminando la clase.

--- clases/09_abstraction.py
+++ clases/09_abstraction.py
@@ -1,25 +1,56 @@
 """
 La abstracción es el concepto de ocultar los detalles y mostrar solo lo esencial, en pocas palabras permite
 al usuario interactuar con una clase sin necesidad de preocuparse de cómo funciona por dentro.
+
+* Clases Abstractas:
+Es una clase que no se puede instanciar pero sirve como plantilla para crear otras clases.
+
+* Implementación:
+Implementar un método quiere decir definir como va a funcionar.
 """
-
 from recursos import tittle_format as tf
+from abc import ABC, abstractclassmethod
 
-class Auto():
-    def __init__(self):
-        self._estado = 'apagado'
+
+class Persona(ABC):
+    @abstractclassmethod
+    def __init__(self, nombre, edad, sexo, actividad):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.actividad = actividad
+
+    @abstractclassmethod
+    def hacer_activiad(self):
+        pass
     
-    def encender(self):
-        self._estado = 'encendido'
-        print('El auto está encendido.')
+    def presentarse(self):
+        print(f'Hola, soy {self.nombre} y tengo {self.edad} años.')
+
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, sexo, actividad):
+        super().__init__(nombre, edad, sexo, actividad)
     
-    def conducir(self):
-        if self._estado == 'apagado':
-            self.encender()
-        print('Conduciendo el auto.')
+    def hacer_activiad(self):
+        print(f'Estoy estudiando: {self.actividad}')
+
+class Trabajador(Persona):
+    def __init__(self, nombre, edad, sexo, actividad):
+        super().__init__(nombre, edad, sexo, actividad)
+    
+    def hacer_activiad(self):
+        print(f'Estoy trabajando como: {self.actividad}')
+
+
 
 
 tf('abstraction')
 
-mi_auto = Auto()
-mi_auto.conducir()
+maestra = Trabajador('Angela', 47, 'F', 'Maestra')
+estudiante = Estudiante('Brayan', 25, 'M', 'Programación')
+
+maestra.presentarse()
+maestra.hacer_activiad()
+
+estudiante.presentarse()
+estudiante.hacer_activiad()
