d3e998d74d15d9f4a29a5e20da608ce5936251e4
Brayan Ellis <ellis@localhost.localdomain>
2025-03-31 19:57:46 -0400

    doc: documentando el código para facilitar el repaso.

--- clases/09_abstraction.py
+++ clases/09_abstraction.py
@@ -9,11 +9,15 @@ Es una clase que no se puede instanciar pero sirve como plantilla para crear otr
 Implementar un método quiere decir definir como va a funcionar.
 """
 from recursos import tittle_format as tf
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractclassmethod # Importamos ABC y abstractmethod para crear una clase abstracta
 
 
 class Persona(ABC):
-    @abstractclassmethod
+    """
+    Clase abstracta que representa una persona.
+    Requiere que cualquier subclase implemente el método hacer_actividad.
+    """
+    @abstractclassmethod # Indicamos que este método debe ser implementado en las subclases
     def __init__(self, nombre, edad, sexo, actividad):
         self.nombre = nombre
         self.edad = edad
@@ -22,16 +26,28 @@ class Persona(ABC):
 
     @abstractclassmethod
     def hacer_activiad(self):
+        """
+        Método abstracto que debe ser implementado por las subclases.
+        """
         pass
     
     def presentarse(self):
         print(f'Hola, soy {self.nombre} y tengo {self.edad} años.')
 
 class Estudiante(Persona):
+    """
+    Representa a un estudiante, que es una subclase de Persona.
+    """
     def __init__(self, nombre, edad, sexo, actividad):
+        """
+        Inicializa un estudiante con los mismos atributos de Persona.
+        """
         super().__init__(nombre, edad, sexo, actividad)
     
     def hacer_activiad(self):
+        """
+        Implementa el método abstracto, describiendo qué estudia el estudiante.
+        """
         print(f'Estoy estudiando: {self.actividad}')
 
 class Trabajador(Persona):
@@ -46,6 +62,7 @@ class Trabajador(Persona):
 
 tf('abstraction')
 
+# Creación de instancias y prueba de los métodos
 maestra = Trabajador('Angela', 47, 'F', 'Maestra')
 estudiante = Estudiante('Brayan', 25, 'M', 'Programación')
 
