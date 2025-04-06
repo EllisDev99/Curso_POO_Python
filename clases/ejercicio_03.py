9c9d74551b6d227b8169321c88e3a485b11fc440
Brayan Ellis <ellis@localhost.localdomain>
2025-04-05 23:29:42 -0400

    doc: definiendo el ejercicio

---
+++ clases/ejercicio_03.py
@@ -0,0 +1,11 @@
+"""
+Crear un juego de fusión
+
+El juego consiste en crear personajes de un juego y que estos personajes se puedan 
+funsionar para formar personajes más poderosos que tengan más poder...
+
+Para ello deberemos cambiar el comportamiento del operador '+' para que cuando los
+personajes se fusionen, salga un nuevo personaje con habilidades mejoradas.
+
+una posible formula es: el promedio de las habilidades de ambos, al cuadrado
+"""
