894476136e8296be1d3e26234a5efad12e519435
Brayan Ellis <ellis@localhost.localdomain>
2025-04-07 19:20:28 -0400

    feat: se anido todo el bloque try dentro de un while y se agrego un else que controla el rango de las opciones elegibles.

--- clases/ejercicio_04.1.py
+++ clases/ejercicio_04.1.py
@@ -46,27 +46,33 @@ Tu programa debe permitir al usuario:
 from recursos import tittle_format as tf
 
 def opciones():
-    try:
-        print('[1] - Crear espada\n[2] - Fusionar espadas\n[3] - Ver espadas creadas\n[4] - Eliminar espada\n[0] - Salir')
-        opcion = int(input('Elíga una opción: '))
-        if opcion == 0:
-            print('Saliendo del creador de espadas.')
-            break
-        elif opcion == 1:
-            pass
-        elif opcion == 2:
-            pass
-        elif opcion == 3:
-            pass
-        elif opcion == 4:
-            pass
+    while True:
+        try:
+            print('[1] - Crear espada\n[2] - Fusionar espadas\n[3] - Ver espadas creadas\n[4] - Eliminar espada\n[0] - Salir')
+            opcion = int(input('Elíga una opción: '))
+            if opcion == 0:
+                print('Saliendo del creador de espadas.')
+                break
+            elif opcion == 1:
+                pass
+            elif opcion == 2:
+                pass
+            elif opcion == 3:
+                pass
+            elif opcion == 4:
+                pass
+            else:
+                print('='*100)
+                print('Valor fuera de rango.\nDigite una opción valida.')
+                print('='*100)
+                    
+        except(ValueError):
+            print('Error con el tipo de dato digitado.')
+            print('Digite una opción valida.')
+        except KeyboardInterrupt:
+                print('Programa interrumpido por el usuario.')
                 
-    except(ValueError):
-        print('Error con el tipo de dato digitado.')
-        print('Digite una opción valida.')
-    except KeyboardInterrupt:
-            print('Programa interrumpido por el usuario.')
-
+    print('Gracias, ¡vuelva pronto!.')
 
 
 
