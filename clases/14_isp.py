1ed88d050832f90492fa08df4c96823213864e02
Brayan Ellis <ellis@localhost.localdomain>
2025-04-21 19:57:36 -0400

    feat: iniciando clase sobre isp.py

---
+++ clases/14_isp.py
@@ -0,0 +1,8 @@
+"""
+Las siglas SOLID representan cinco principios fundamentales de la programación orientada a objetos que ayudan a escribir código más mantenible, escalable y limpio. 
+
+🔹 I - Interface Segregation Principle (ISP)
+'Es mejor tener muchas interfaces específicas que una sola general que haga de todo.'
+
+DEFINICIÖN DADA POR MI AMIGA CHATGPT
+"""
