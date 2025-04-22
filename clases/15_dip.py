22b9011fa457053e080d13eeb98b86d9621f2340
Brayan Ellis <ellis@localhost.localdomain>
2025-04-21 20:33:55 -0400

    feat: iniciando la clase sobre dip

---
+++ clases/15_dip.py
@@ -0,0 +1,10 @@
+"""
+Las siglas SOLID representan cinco principios fundamentales de la programación orientada a objetos que ayudan a escribir código más mantenible, escalable y limpio. 
+
+🔹 D - Dependency Inversion Principle (DIP)
+"Depende de abstracciones, no de concreciones."
+
+Las clases no deberían depender de clases concretas, sino de interfaces o abstracciones. Porque si cambiás la implementación, no deberías tener que cambiar la clase que la usa. Básicamente: no te cases con los detalles, enamorate del concepto.
+
+DEFINICIÖN DADA POR MI AMIGA CHATGPT
+"""
