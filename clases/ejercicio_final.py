1c7e02b782e3fbfab81d6687b85a3154968443c6
Brayan Ellis <ellis@localhost.localdomain>
2025-04-23 19:37:05 -0400

    feat: preparando fichero para el proyecto final

---
+++ clases/ejercicio_final.py
@@ -0,0 +1,7 @@
+"""
+Chatbot analizador de sentimientos
+
+En este proyecto, podrías desarrollar un chatbot en python, que nos pida que le digamos algo
+y tome eso que le decimos, analice el sentimiento y nos responda cual es el sentimiento.
+
+"""
